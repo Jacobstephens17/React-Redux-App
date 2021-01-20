9040335d4a06366ec628ca087e38b239bf01d1a2
Jacob Stephens <jacobstephens2@gmail.com>
2021-01-20 15:41:23 -0800

    finished mvp i believej

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import MostWanted from './components/MostWanted';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div>
+    <StyledDiv>
       <MostWanted/>
-    </div>
+    </StyledDiv>
   );
 }
 
 export default App;
+const StyledDiv = styled.div`
+display:flex;
+background-color:black;
+align-items:center;
+flex-direction:column;
+
+`
--- react-redux-app/src/components/MostWanted.js
+++ react-redux-app/src/components/MostWanted.js
@@ -5,27 +5,25 @@ import styled from 'styled-components';
 
 const MostWanted = ({image, isFetching, error, getMostWanted}) => {
     useEffect(() => {
-        getMostWanted();
       }, []);
       
-    const handleClick = (e) => {
-        e.preventDefault();
+    const handleClick = () => {
         getMostWanted();
     }
 
     if (error) {
-        return <h2>We encountered an error... {error}</h2>;
+        return <H2>We encountered an error... {error}</H2>;
       }
     
     if (isFetching) {
-        return <h2>Fetching Data...</h2>; 
+        return <H2>Fetching Data...</H2>; 
     }
 
     return(
         <StyledDiv>
-            <h1>Random Image Below:</h1> 
-            <Image imgUrl={image} />
+            <H1>Random Image Below:</H1> 
             <Button onClick={handleClick}>Get Image</Button>
+            <Image imgUrl={image} />
         </StyledDiv>
     )
 }
@@ -40,26 +38,44 @@ const mapStateToProps = state => {
 
 export default connect(mapStateToProps, { getMostWanted })(MostWanted);
 
-const Image = styled.div`
-    width: 100%;
-    height:900px;
-    background: url(${(props)=>props.imgUrl}) no-repeat; 
-    background-size:500px;
-`
-
 const StyledDiv = styled.div`
     display:flex;
     flex-direction:column;
-    margin:0 auto;
-    width:25%;
-    justify-content:center;
-    justify-items:center;
     align-items:center;
     align-content:center;
+    justify-content:center;
 `
 
+const Image = styled.div`
+    width: 100%;
+    display:flex;
+    flex-direction:column;
+    align-content:center;
+    height:900px;
+    justify-content:center;
+    align-items:center;
+    justify-items:center;
+    margin:0 auto;
+    background: url(${(props)=>props.imgUrl}) no-repeat; 
+    background-size:500px;
+    
+`
+const H1 = styled.h1`
+    color:white;
+    font-size:4rem;
+    text-align:center;
+    font-weight:bolder;
+`
 const Button = styled.button`
     padding:2rem 5rem;
     border-radius:4px;
     margin-top:0%;
+    font-size:1rem;
+`
+
+const H2 = styled.h2`
+color:white;
+background-color:black;
+width:100%;
+height:auto;
 `
